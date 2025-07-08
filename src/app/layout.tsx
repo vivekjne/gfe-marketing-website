bf4fab4d39a76e4c686bc007cb3e1b865751915b
Vivek Joy <vivekjne@gmail.com>
2025-07-08 10:26:36 +0530

    removed unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Noto_Sans } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/layouts/Navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const notoSans = Noto_Sans({
   variable: "--font-noto-sans",
